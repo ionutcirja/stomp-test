df7d620a1f46de11cd2fa3e311ea1980956baa20
Ionut Cirja <ionut.cirja@gmail.com>
2016-09-10 15:25:11 +0100

    plugin app main module

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
 require('./site/index.html');
 require('./site/style.css');
-
-global.DEBUG = false;
-
-const url = 'ws://localhost:8011/stomp';
-const client = window.Stomp.client(url);
-client.debug = (msg) => {
-	if (global.DEBUG) {
-		console.info(msg);
-	}
-};
-
-function connectCallback() {
-	document.getElementById('stomp-status').innerHTML = 'It has now successfully connected to a stomp server ' +
-		'serving price updates for some foreign exchange currency pairs.';
-}
-
-client.connect({}, connectCallback, (error) => {
-	console.log(error.headers.message);
-});
-
-const exampleSparkline = document.getElementById('example-sparkline');
-window.Sparkline.draw(exampleSparkline, [1, 2, 3, 6, 8, 20, 2, 2, 4, 2, 3]);
+require('./es6/app');
