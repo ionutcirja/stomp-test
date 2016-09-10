f6652952918ea9a3fce0e73d76963d09426170de
Ionut Cirja <ionut.cirja@gmail.com>
2016-09-10 13:32:25 +0100

    fix index linting

--- index.js
+++ index.js
@@ -1,36 +1,24 @@
-/**
- * This javascript file will constitute the entry point of your solution.
- *
- * Edit it as you need.  It currently contains things that you might find helpful to get started.
- */
+require('./site/index.html');
+require('./site/style.css');
 
-// This is not really required, but means that changes to index.html will cause a reload.
-require('./site/index.html')
-// Apply the styles in style.css to the page.
-require('./site/style.css')
+global.DEBUG = false;
 
-// if you want to use es6, you can do something like
-//     require('./es6/myEs6code')
-// here to load the myEs6code.js file, and it will be automatically transpiled.
-
-// Change this to get detailed logging from the stomp library
-global.DEBUG = false
-
-const url = "ws://localhost:8011/stomp"
-const client = Stomp.client(url)
-client.debug = function(msg) {
-  if (global.DEBUG) {
-    console.info(msg)
-  }
-}
+const url = 'ws://localhost:8011/stomp';
+const client = window.Stomp.client(url);
+client.debug = (msg) => {
+	if (global.DEBUG) {
+		console.info(msg);
+	}
+};
 
 function connectCallback() {
-  document.getElementById('stomp-status').innerHTML = "It has now successfully connected to a stomp server serving price updates for some foreign exchange currency pairs."
+	document.getElementById('stomp-status').innerHTML = 'It has now successfully connected to a stomp server ' +
+		'serving price updates for some foreign exchange currency pairs.';
 }
 
-client.connect({}, connectCallback, function(error) {
-  alert(error.headers.message)
-})
+client.connect({}, connectCallback, (error) => {
+	console.log(error.headers.message);
+});
 
-const exampleSparkline = document.getElementById('example-sparkline')
-Sparkline.draw(exampleSparkline, [1, 2, 3, 6, 8, 20, 2, 2, 4, 2, 3])
+const exampleSparkline = document.getElementById('example-sparkline');
+window.Sparkline.draw(exampleSparkline, [1, 2, 3, 6, 8, 20, 2, 2, 4, 2, 3]);
